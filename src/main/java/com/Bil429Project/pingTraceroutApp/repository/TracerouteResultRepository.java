99861402385ad8942d89d5e574e70a65002e7fba
Mila-U <88654285+Mila-U@users.noreply.github.com>
2023-11-27 18:33:40 +0300

    commit: initial commit for tracert

---
+++ src/main/java/com/Bil429Project/pingTraceroutApp/repository/TracerouteResultRepository.java
@@ -0,0 +1,8 @@
+package com.Bil429Project.pingTraceroutApp.repository;
+
+import com.Bil429Project.pingTraceroutApp.entity.TracerouteResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TracerouteResultRepository extends JpaRepository<TracerouteResult, Long> {
+}
+
